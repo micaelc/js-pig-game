666d89eeb9bf3c038fbf2421b630ab617ffbe9c9
Micael Campos <micael.campos@gmail.com>
2018-04-27 17:16:32 +0100

    reset scores

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ GAME RULES:
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
-
 */
-
 
 var scores, roundSscore, activePlayer;
 
@@ -18,6 +16,10 @@ activePlayer = 0;
 
 // hide dice
 document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
@@ -25,7 +27,8 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     
     // diceDOM
     var diceDom = document.querySelector('.dice');
-
+    
+    // show dice acording to the random number
     diceDom.style.display = 'block';
     diceDom.src = 'img/dice-' + dice + '.png';
 
