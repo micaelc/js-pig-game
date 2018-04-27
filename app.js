16471984c89664cd5c0bb7ca743941252175b0c4
Micael Campos <micael.campos@gmail.com>
2018-04-27 17:35:57 +0100

    change active player

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ activePlayer = 0;
 
 // hide dice
 document.querySelector('.dice').style.display = 'none';
+// Reset Game
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
@@ -24,13 +25,30 @@ document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
     var dice = Math.floor(Math.random() * 6) + 1
-    
+
     // diceDOM
     var diceDom = document.querySelector('.dice');
-    
+
     // show dice acording to the random number
     diceDom.style.display = 'block';
     diceDom.src = 'img/dice-' + dice + '.png';
 
-    // 
+    // Update the roundScore if the number rolled is > 1
+    if (dice !== 1) {
+        // Add Score 
+        roundSscore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundSscore;
+
+    } else {
+        // Next Player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+        // reset current Scores
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        // toggle active classs from player panel
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        // hide dice
+        document.querySelector('.dice').style.display = 'none';
+    }
 });
